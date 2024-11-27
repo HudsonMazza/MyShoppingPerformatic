8f83981bb7aed0d80b9c432763b6843a732b6f3a
Hudson Mazza <mazzahudson05@outlook.com>
2024-11-27 14:06:11 -0300

    first commit

--- index.js
+++ index.js
@@ -5,5 +5,14 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import { Platform } from 'react-native';
 
+if (Platform.OS === 'web') {
+    AppRegistry.registerComponent(appName, () => App);
+    AppRegistry.runApplication(appName, {
+      initialProps: {},
+      rootTag: document.getElementById('app-root'),
+    });
+  }
+  
 AppRegistry.registerComponent(appName, () => App);
